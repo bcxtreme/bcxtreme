df21cb74b691fa21fe6a0b2078d5017c067576a9
Jeremy Salwen <jeremysalwen@gmail.com>
2011-11-17 15:15:17 -0500

    Use 'hello' as test message, as bitcoin wiki gives it as double-sha example

--- top/top.sv
+++ top/top.sv
@@ -13,77 +13,29 @@ initial begin
   clk=0;
   rst=1;
   //W0
-  M=32'h61626364;
+  M=32'h68656c6c;
   #1 clk=1;
   #1 clk=0;
   #1 clk=1;
   #1 clk=0;
   rst=0;
   //W1
-  M=32'h62636465;
+  M=32'h6f800000;
   #1 clk=1;
   #1 clk=0;
-  M=32'h63646566;
-  #1 clk=1;
-  #1 clk=0;
-  M=32'h64656667;
-  #1 clk=1;
-  #1 clk=0;
-  M=32'h65666768;
-  #1 clk=1;
-  #1 clk=0;
-  M=32'h66676869;
-  #1 clk=1;
-  #1 clk=0;
-  M=32'h6768696a;
-  #1 clk=1;
-  #1 clk=0;
-  M=32'h68696a6b;
-  #1 clk=1;
-  #1 clk=0;
-  M=32'h696a6b6c;
-  #1 clk=1;
-  #1 clk=0;
-  M=32'h6a6b6c6d;
-  #1 clk=1;
-  #1 clk=0;
-  M=32'h6b6c6d6e;
-  #1 clk=1;
-  #1 clk=0;
-  M=32'h6c6d6e6f;
-  #1 clk=1;
-  #1 clk=0;
-  M=32'h6d6e6f70;
-  #1 clk=1;
-  #1 clk=0;
-  M=32'h6e6f7071;
-  #1 clk=1;
-  #1 clk=0;
-  M=32'h80000000;
-  #1 clk=1;
-  #1 clk=0;
-  M=32'h00000000;
+  M=0;
+  for(int i=2; i<15; i++) begin
+    #1 clk=1;
+    #1 clk=0;
+  end
+  M=32'h00000028;
   #1 clk=1;
   #1 clk=0;
   for(int i=16; i<64; i++) begin
     #1 clk=1;
     #1 clk=0;
   end
-  //W0 through W14
-  M=32'h0;
-  for(int i=0; i<15; i++) begin
-    #1 clk=1;
-    #1 clk=0;
-  end
-  //W15
-  M=32'h000001c0;
-  #1 clk=1;
-  #1 clk=0;
-  //W16 through W63
-  for(int i=16; i<64; i++) begin
-    #1 clk=1;
-    #1 clk=0;
-  end
+
   #1 $display("%x%x%x%x%x%x%x%x",hash.a,hash.b,hash.c,hash.d,hash.e,hash.f,hash.g,hash.h);
 end
 endmodule
