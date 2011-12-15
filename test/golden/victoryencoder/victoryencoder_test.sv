dfa1e8631a966b63f197589f52c974f393d7056c
Rouault <rff2111@columbia.edu>
2011-12-14 19:42:46 -0500

    modified victoryencoder_test.sv

--- test/golden/victoryencoder/victoryencoder_test.sv
+++ test/golden/victoryencoder/victoryencoder_test.sv
@@ -14,27 +14,40 @@ class encoder_test;
 	bit valid_out;
 	bit success_out;
 	bit [31:0] nonce = 0;
+	
+	int number_of_hashes = 0;
 
 	function void victoryencoder_result();
+		if(success_in0 && succes_in1 && success_in2) begin
+			success_out = 1;
+		end
+		else begin
+			success_out = 0;
+		end
+	
 		if(valid_in0 && newblock_in0) begin
 			//check the hashes
 			if(success_in0) begin
-				nonce = nonce + 1;
+				number_of_hashes = number_of_hashes + 1;
 			end
 		end
 
 		if(valid_in1 && newblock_in1) begin
 			if(success_in1) begin
-				nonce = nonce + 1;
+				number_of_hashes = number_of_hashes + 1;
 			end
 		end
 
 		if(valid_in2 && newblock_in2) begin
 			if(success_in2) begin
-				nonce = nonce + 1;
+				number_of_hashes = number_of_hashes + 1;
 			end
 		end
 		
 		valid_out = 1;
+		
+		if(~(valid_out && success_out)) begin
+			nonce = 0;
+		end
 	endfunction
 endclass
