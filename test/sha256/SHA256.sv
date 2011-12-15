1ef26c542b72d06857203d9ecf923ecafbad9fd6
cd rubin <cdr2104@columbia.edu>
2011-12-15 18:35:16 -0500

    Added single-round function

--- test/sha256/SHA256.sv
+++ test/sha256/SHA256.sv
@@ -179,6 +179,7 @@ function automatic bit[0:255] sha256( string message );
     //  b := a
     //  a := t1 + t2
     for ( int i = 0; i < 64; i++ ) begin
+      /*
       int s0 = rightrotate( a, 2 ) ^ rightrotate( a, 13 ) ^ rightrotate( a, 22 );
       int maj = (a & b) ^ (a & c) ^ (b & c);
       int t2 = s0 + maj;
@@ -194,6 +195,8 @@ function automatic bit[0:255] sha256( string message );
       c = b;
       b = a;
       a = t1 + t2;
+      */
+      single_round( a, b, c, d, e, f, g, h, i, w[i] );
     end
 
 
@@ -224,7 +227,44 @@ function automatic bit[0:255] sha256( string message );
   //$display( "%h", result );
 
 endfunction
-           
+  
+
+function automatic bit[255:0] sha256_other( string message );
+  return sha256( message );
+endfunction 
+
+
+function automatic single_round( ref bit[0:31] a, ref bit[0:31] b, ref bit[0:31] c, ref bit[0:31] d, ref bit[0:31] e, ref bit[0:31] f, ref bit[0:31] g, ref bit[0:31] h, int round_number, bit[0:31] round_word );
+
+  bit [0:31] _k[64] = {
+    32'h428a2f98, 32'h71374491, 32'hb5c0fbcf, 32'he9b5dba5, 32'h3956c25b, 32'h59f111f1, 32'h923f82a4, 32'hab1c5ed5,
+    32'hd807aa98, 32'h12835b01, 32'h243185be, 32'h550c7dc3, 32'h72be5d74, 32'h80deb1fe, 32'h9bdc06a7, 32'hc19bf174,
+    32'he49b69c1, 32'hefbe4786, 32'h0fc19dc6, 32'h240ca1cc, 32'h2de92c6f, 32'h4a7484aa, 32'h5cb0a9dc, 32'h76f988da,
+    32'h983e5152, 32'ha831c66d, 32'hb00327c8, 32'hbf597fc7, 32'hc6e00bf3, 32'hd5a79147, 32'h06ca6351, 32'h14292967,
+    32'h27b70a85, 32'h2e1b2138, 32'h4d2c6dfc, 32'h53380d13, 32'h650a7354, 32'h766a0abb, 32'h81c2c92e, 32'h92722c85,
+    32'ha2bfe8a1, 32'ha81a664b, 32'hc24b8b70, 32'hc76c51a3, 32'hd192e819, 32'hd6990624, 32'hf40e3585, 32'h106aa070,
+    32'h19a4c116, 32'h1e376c08, 32'h2748774c, 32'h34b0bcb5, 32'h391c0cb3, 32'h4ed8aa4a, 32'h5b9cca4f, 32'h682e6ff3,
+    32'h748f82ee, 32'h78a5636f, 32'h84c87814, 32'h8cc70208, 32'h90befffa, 32'ha4506ceb, 32'hbef9a3f7, 32'hc67178f2
+  };
+
+  int s0 = rightrotate( a, 2 ) ^ rightrotate( a, 13 ) ^ rightrotate( a, 22 );
+  int maj = (a & b) ^ (a & c) ^ (b & c);
+  int t2 = s0 + maj;
+  int s1 = rightrotate( e, 6 ) ^ rightrotate( e, 11 ) ^ rightrotate( e, 25);
+  int ch = (e & f) ^ ((~ e) & g);
+  int t1 = h + s1 + ch + _k[round_number] + round_word;
+
+  h = g;
+  g = f;
+  f = e;
+  e = d + t1;
+  d = c;
+  c = b;
+  b = a;
+  a = t1 + t2;
+
+endfunction
+
 
 function string binary_array_to_string( bit _array[] );
   string result;
--- test/sha256/test.sv
+++ test/sha256/test.sv
@@ -7,16 +7,16 @@ initial begin
   string message;
 
   message = "";
-  $display( "%s\n%h", message, sha256( message ) );
+  $display( "%s\n%h\n%h", message, sha256( message ), sha256_other( message ) );
 
   message = "Hello";
-  $display( "%s\n%h", message, sha256( message ) );
+  $display( "%s\n%h\n%h", message, sha256( message ), sha256_other( message ) );
 
   message = "The quick brown fox jumps over the lazy dog";
-  $display( "%s\n%h", message, sha256( message ) );
+  $display( "%s\n%h\n%h", message, sha256( message ), sha256_other( message ) );
 
   message = "The quick brown fox jumps over the lazy dog.";
-  $display( "%s\n%h", message, sha256( message ) );
+  $display( "%s\n%h\n%h", message, sha256( message ), sha256_other( message ) );
 
    
 end
