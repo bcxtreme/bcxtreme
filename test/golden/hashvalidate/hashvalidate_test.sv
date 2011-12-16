69934f67f51d2459d3d72272e58015b1d47a39c4
Rouault <rff2111@columbia.edu>
2011-12-15 21:47:35 -0500

    updated hashvalidate_test.sv, removed uneccessary blockstorage_test.sv file

--- test/golden/hashvalidate/hashvalidate_test.sv
+++ test/golden/hashvalidate/hashvalidate_test.sv
@@ -3,15 +3,13 @@ class hashvalidate_test;
 	bit valid_in;
 	bit newblock_in;
 	bit [255:0] hash;
-
-	bit [255:0] max_target = 256'h00000000FFFF0000000000000000000000000000000000000000000000000000;
+	
+	bit [255:0] le_hash;
 	
 	bit [255:0] target;
 	bit valid_out;
 	bit newblock_out;
 	bit success;
-
-	bit[255:0] diff;//difficulty max_target / current_target
 
 	function void get_target();
 		//calculate 256 bit target using diffuclty
@@ -24,9 +22,44 @@ class hashvalidate_test;
 		
 		target =  le_diff[23:0]* 2 **(8*(le_diff[31:24]-3));
 
-		diff = max_target / target;
-
 		valid_out = 1;
+	endfunction
+	
+
+	function void reverse_hash();
+		le_hash[255:248] = hash[7:0];
+		le_hash[247:240] = hash[15:8];
+		le_hash[239:232] = hash[23:16];
+		le_hash[231:224] = hash[31:24];
+		le_hash[223:216] = hash[39:32];
+		le_hash[215:208] = hash[47:40];
+		le_hash[207:200] = hash[55:48];
+		le_hash[199:192] = hash[63:56];
+		le_hash[191:184] = hash[71:65];
+		le_hash[191:184] = hash[79:72];
+		le_hash[183:176] = hash[87:80];
+		le_hash[175:168] = hash[95:88];
+		le_hash[167:160] = hash[103:96];
+		le_hash[159:152] = hash[111:104];
+		le_hash[151:144] = hash[119:112];
+		le_hash[135:128] = hash[127:120];
+		le_hash[127:120] = hash[135:128];
+		le_hash[119:112] = hash[143:136];
+		le_hash[111:104] = hash[151:144];
+		le_hash[103:96] = hash[159:152];
+		le_hash[95:88] = hash[167:160];
+		le_hash[87:80] = hash[175:168];
+		le_hash[79:72] = hash[183:176];
+		le_hash[71:65] = hash[191:184];
+		le_hash[63:56] = hash[199:192];
+		le_hash[55:48] = hash[207:200];
+		le_hash[47:40] = hash[215:208];
+		le_hash[39:32] = hash[223:216];
+		le_hash[31:24] = hash[231:224];
+		le_hash[23:16] = hash[239:232];
+		le_hash[15:8] = hash[247:240];
+		le_hash[7:0] = hash[255:248];
+
 	endfunction
 	
 	function void hashvalidate_result();
@@ -36,9 +69,9 @@ class hashvalidate_test;
 			end
 			
 			get_target();
-			
+			reverse_hash(); //need to byte reverse the hash before comparing
 
-			if(hash < diff) begin //check if the output matches difficulty
+			if(le_hash < target) begin //check if the output matches difficulty
 				success = 1;
 			end
 
--- test/golden/hashvalidate/target_test.sv
+++ test/golden/hashvalidate/target_test.sv
@@ -1,17 +1,21 @@
 program target_test();
 	
 	bit[31:0] D;
+	bit[255:0] X;
 	hashvalidate_test gettarget;
 initial begin	
 	gettarget = new();
 	
 
 	D = 'hcb04041b;
-	//$display("%x",D);
+	X = 256'h00000000000404cb000000000000000000000000000000000000000000000000;
+
+//	$display("%x",X);
 		
 	
-	//gettarget.get_target();
+	//gettarget.reverse_hash(X);
 end
 
 
 endprogram
+
