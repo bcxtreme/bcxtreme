f51d2081710bad2e43ead94862309e398e6b4bbe
Robert Martin <rdm2128@columbia.edu>
2011-12-16 21:16:53 -0500

    Commit updated HashValidator with bugs removed, and added to Makefile (not hooked up)

--- src/implementation/standard_hash_validator.sv
+++ src/implementation/standard_hash_validator.sv
@@ -1,31 +1,33 @@
+
 module standard_hash_validator(
-input logic clk,
-input logic[255:0] hash,
-input logic[31:0] difficulty,
-output logic success
+	input logic clk,
+	input logic[255:0] hash,
+	input logic[31:0] difficulty,
+	output logic success
 );
-logic succ;
-ff #(.WIDTH(1)) valid(.clk,.data_i(succ),.data_o(success));
+	logic succ;
+	ff #(.WIDTH(1)) valid(.clk,.data_i(succ),.data_o(success));
 
-logic[255:0] hash_as_number;
+	logic[255:0] hash_as_number;
+	generate 
+	for(genvar i=0; i<32; i++) begin
+		assign hash_as_number[8*i+7 : 8*i]=hash[255-i*8 : 248-i*8];
+	end
+	endgenerate
 
-generate 
-for(genvar i=0; i<32; i++) begin
-  assign hash_as_number[(i+1)*8:i*8]=hash[256-i*8:256-(i+1)*8];
-end
-endgenerate
+	logic[23:0] mantissa_as_number;
+	generate
+	for(genvar i=0; i<3; i++) begin
+		assign mantissa_as_number[8*i+7 : 8*i]=difficulty[31-i*8 : 24-i*8];
+	end
+	endgenerate
 
-logic[23:0] mantissa_as_number;
-for(genvar i=0; i<3; i++) begin
-  assign mantissa_as_number[(i+1)*8:i*8]=difficulty[24-i*8:24-(i+1)*8];
-end
+	logic[7:0] exp;
+	assign exp=difficulty[31:24];
 
-logic[7:0] exp;
-assign exp=difficulty[31:24];
+	logic[255:0] target;
+	assign target = mantissa_as_number << 8*(exp-3);
 
-logic[255:0] target;
-assign target=mantissa_as_number << 8*(exp-3);
-
-assign succ= hash_as_number <= target;
+	assign succ = hash_as_number <= target;
 
 endmodule
