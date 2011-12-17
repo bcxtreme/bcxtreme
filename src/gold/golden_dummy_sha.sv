5034060ca99b4a718bae8a00e79472de12b66f9b
Robert Martin <rdm2128@columbia.edu>
2011-12-16 23:05:42 -0500

    Change "dummy-sha" so that lower 256 bits are the hash and the last 32 bits are the difficulty.

--- src/gold/golden_dummy_sha.sv
+++ src/gold/golden_dummy_sha.sv
@@ -14,12 +14,14 @@ class golden_dummy_sha #(parameter COUNTBITS = 6, parameter DELAY_C = 10);
 
 	// Internal State
 	local bit[255:0] hash_buf[DELAY_C + 1];
+	local bit[32:0] dif_buf[DELAY_C + 1];
 	local bit valid_buf[DELAY_C + 1];
 	local bit new_buf[DELAY_C + 1];
 
 	function new();
 		for (int i = 0; i <= DELAY_C; i++) begin
 			hash_buf[i] = 0;
+			dif_buf[i] = 0;
 			valid_buf[i] = 1'b0;
 			new_buf[i] = 1'b0;
 		end
@@ -29,20 +31,20 @@ class golden_dummy_sha #(parameter COUNTBITS = 6, parameter DELAY_C = 10);
 	task cycle();
 		for (int i = DELAY_C; i > 0; i--) begin
 			valid_buf[i] = valid_buf[i - 1];
+			dif_buf[i] = dif_buf[i - 1];
 			hash_buf[i] = hash_buf[i - 1];
 			new_buf[i] = new_buf[i - 1];
 		end
 
 		valid_buf[0] = validIn_i;
-		hash_buf[0] = initialState_i[255:0] ^ initialState_i[351:256];
+		dif_buf[0] = initialState_i[351:320];
+		hash_buf[0] = initialState_i[255:0];
 		new_buf[0] = newBlockIn_i;
 
-
 		validOut_o = valid_buf[DELAY_C];
+		difficulty_o = dif_buf[DELAY_C];
 		newBlockOut_o = new_buf[DELAY_C];
 		hash_o = hash_buf[DELAY_C];
-		difficulty_o = 'hf2b9441a; // TODO: calculate difficulty instead of hard-coding it!
-		// $display("[ %b, %b, %b, %b ]",  valid_buf[DELAY_C - 3],  valid_buf[DELAY_C - 2],  valid_buf[DELAY_C - 1], valid_buf[DELAY_C]);
 
 	endtask
 
--- src/implementation/dummy_sha.sv
+++ src/implementation/dummy_sha.sv
@@ -11,20 +11,20 @@ module dummy_sha #(parameter COUNTBITS=6, parameter DELAY_C = 10) (
 	output[31:0] difficulty
 );
 
+	wire [255:0] hash_in;
+	wire [31:0] difficult_in;
 
-	// XOR the initial stage on top of itself:
-	wire [255:0] upper_half, lower_half;
-	logic [255:0] hash_in;
-	assign lower_half[255:0] = initialState[255:0];
-	assign upper_half[95:0] = initialState[351:256];
-	assign upper_half[255:96] = 0;
-	assign hash_in = upper_half ^ lower_half;
+	// The "hash" is the lower 256 bits of the input. The difficulty is the final 32 bits
+	assign hash_in = initialState[255:0];
+	assign difficulty_in = initialState[351:320];
 
 	// Store the value for DELAY_C cycles
 	wire [255:0] trans[DELAY_C + 1];
+	wire [31:0] trans_difficulty[DELAY_C + 1];
 	wire trans_valid[DELAY_C + 1];
 	wire trans_new[DELAY_C + 1];
 	assign trans[0] = hash_in;
+	assign trans_difficulty[0] = difficulty_in;
 	assign trans_valid[0] = validIn;
 	assign trans_new[0] = newBlockIn;
 	for (genvar n = 0; n < DELAY_C; n++) begin
@@ -34,6 +34,13 @@ module dummy_sha #(parameter COUNTBITS=6, parameter DELAY_C = 10) (
 			.enable(1'b1),
 			.data_i(trans[n]),
 			.data_o(trans[n + 1])
+		);
+		eff #(.WIDTH(32)) dif (
+			.clk,
+			.rst,
+			.enable(1'b1),
+			.data_i(trans_difficulty[n]),
+			.data_o(trans_difficulty[n + 1])
 		);
 		eff bnew (
 			.clk,
@@ -51,10 +58,9 @@ module dummy_sha #(parameter COUNTBITS=6, parameter DELAY_C = 10) (
 		);
 	end
 	assign hash = trans[DELAY_C];
+	assign difficulty = trans_difficulty[DELAY_C];
 	assign validOut = trans_valid[DELAY_C];
 	assign newBlockOut = trans_new[DELAY_C];
-
-	assign difficulty = 0;
 
 endmodule
 
