236de83f8063c04072389867fa9b13bc588aa9a8
cd rubin <cdr2104@columbia.edu>
2011-12-16 21:01:48 -0500

    Added some structure...

--- src/gold/golden_sha.sv
+++ src/gold/golden_sha.sv
@@ -1,18 +1,47 @@
 
 class golden_sha;
 
-  
-  bit data[];
+  bit[31:0] _h[8] = {
+    32'h6a09e667, 32'hbb67ae85, 32'h3c6ef372, 32'ha54ff53a, 32'h510e527f, 32'h9b05688c, 32'h1f83d9ab, 32'h5be0cd19
+  };
 
-  function new ( bit valid, bit newBlock, bit[512:0] initialState, bit[31:0] w1, bit[31:0] w2, bit[31:0] w3 ) 
-    
-    data = new[1024](initialState);
-    
+  int _nonce;
 
+  bit _valid;
+  bit _newBlock;
+  bit _initialState[];
+
+  bit [255:0] _result;
+
+  function setValid( bit valid );
+    _valid = valid;
+  endfunction
+
+  function setNewBlock( bit newBlock );
+    _newBlock = newBlock;
   endfunction  
 
+  function setInitialState( bit initialState[] );
+    _initialState = initialState;
+  endfunction
 
+  function evaluate();
 
+    int round_number = 1;  
+
+    if ( _valid && _newBlock )
+      _nonce = 0;
+    else
+      _nonce += 1;
+
+    // if you want to skip a round _h will need to contain the results of the first round
+    _result = bitcoin_sha256( _h, _initialState, round_number );
+  endfunction
+
+  function getResult();
+    // should only make result available after a particular time
+    return _result;
+  endfunction
 
 endclass
 
