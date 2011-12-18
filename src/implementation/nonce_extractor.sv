ad70e0e337df6182ec5ae77bc5cb2662d5d37c1b
Jeremy Salwen <jeremysalwen@gmail.com>
2011-12-18 03:25:19 -0500

    Fix nonce_extractor

--- src/implementation/nonce_extractor.sv
+++ src/implementation/nonce_extractor.sv
@@ -1,13 +1,20 @@
-module nonce_extractor #(parameter PARTITIONBITS=1)(
+module nonce_extractor #(parameter NUMPROCESSORS=1, parameter NONCESPACE=(1<<6),parameter PARTITIONBITS=$clog(NUMPROCESSORS),parameter NONCESPERPROCESSOR=NONCESPACE/NUMPROCESSORS, parameter LEFTOVER=NONCESPACE-NONCESPERPROCESSOR*NUMPROCESSORS)(
 input logic clk,
-input logic newblock,
+input logic rst,
+input logic valid_i,
+input logic newblock_i,
+input logic success_i,
+output logic valid_o,
+output logic newblock_o,
+output logic success_o,
 input logic[PARTITIONBITS-1:0] processor_index,
 output logic[31:0] nonce);
 
 logic[31:0] base_nonce;
+logic[31:0] numextra;
 always_comb begin
-  base_nonce= processor_index <<(32-PARTITIONBITS);
-  //TODO implement more complex schemes for non-power-of-two partitioning.
+  numextra=(processor_index>LEFTOVER)?LEFTOVER:processor_index;
+  base_nonce= NONCESPERPROCESSOR*processor_index+numextra;
 end 
  
 logic[31:0] count_old;
@@ -15,9 +22,13 @@ logic[31:0] count_new;
 ff #(.WIDTH(32)) f(.clk,.data_i(count_new),.data_o(count_old));
 
 logic[31:0] calculated_count;
-assign calculated_count=newblock?0:count_old;
-assign count_new=calculated_count+1'd1;
+assign count_new=(newblock&valid_i)?0:(count_old+valid_i);
 
-assign nonce=calculated_count+base_nonce;
+logic[31:0] nonce_tmp;
+assign nonce_tmp=new_count+base_nonce;
 
+rff #(.WIDTH(1)) vff(.clk,.rst,.data_i(valid_i),.data_o(valid_o));
+rff #(.WIDTH(1)) nbff(.clk,.rst,.data_i(newblock_i),.data_o(newblock_o));
+rff #(.WIDTH(1)) sff(.clk,.rst,.data_i(success_i),.data_o(success_o));
+rff #(.WIDTH(32)) nff(.clk,.rst,.data_i(nonce_tmp),.data_o(nonce));
 endmodule
