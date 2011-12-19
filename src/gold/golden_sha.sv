8123fca5e999f0a2d29e558a970216378d577f48
Robert Martin <rdm2128@columbia.edu>
2011-12-18 21:54:55 -0500

    Hook up golden sha, but dummy sha behavior is still in effect.

--- src/gold/golden_sha.sv
+++ src/gold/golden_sha.sv
@@ -1,29 +1,57 @@
 
+class golden_sha #(parameter DELAY_C = 10, parameter PROCESSORINDEX=0, parameter NUMPROCESSORS=1);
+
+	// Inputs
+	bit validIn_i;
+	bit newBlockIn_i;
+	bit[351:0] initialState_i;
+
+	// Outputs
+	bit validOut_o;
+	bit newBlockOut_o;
+	bit[255:0] hash_o;
+	bit[31:0] difficulty_o;
+
+	// Internal State
+	local bit[255:0] hash_buf[DELAY_C + 1];
+	local bit[31:0] dif_buf[DELAY_C + 1];
+	local bit valid_buf[DELAY_C + 1];
+	local bit new_buf[DELAY_C + 1];
+	local bit[31:0] nonce;
+
+/*
+	bit[31:0] _h[8] = {
+   	 32'h6a09e667, 32'hbb67ae85, 32'h3c6ef372, 32'ha54ff53a, 32'h510e527f, 32'h9b05688c, 32'h1f83d9ab, 32'h5be0cd19
+ 	 };
+  
+ 	 //coreInputsIfc.reader in
+
+	bit _valid;
+	bit _newBlock;
+ 	bit [511:0] _firstChunk;
+
+ 	bit [31:0] _w1;
+ 	bit [31:0] _w2;
+	bit [31:0] _w3;
+ 	bit [31:0] _nonce;
 
 
+  	bit [255:0] _result;
+*/
 
-class golden_sha;
+	function new();
+		// Initialize buffers
+		for (int i = 0; i <= DELAY_C; i++) begin
+			hash_buf[i] = 0;
+			dif_buf[i] = 0;
+			valid_buf[i] = 1'b0;
+			new_buf[i] = 1'b0;
+		end
+		nonce = PROCESSORINDEX;
+	endfunction
 
   
-  bit[31:0] _h[8] = {
-    32'h6a09e667, 32'hbb67ae85, 32'h3c6ef372, 32'ha54ff53a, 32'h510e527f, 32'h9b05688c, 32'h1f83d9ab, 32'h5be0cd19
-  };
-  
-  //coreInputsIfc.reader in
-
-  bit _valid;
-  bit _newBlock;
-  bit [511:0] _firstChunk;
-
-  bit [31:0] _w1;
-  bit [31:0] _w2;
-  bit [31:0] _w3;
-  bit [31:0] _nonce;
-
-
-  bit [255:0] _result;
-
-  
+/*
   function configure( virtual coreInputsIfc in );
     _valid = in.valid;
     _newBlock = in.newblock;
@@ -41,13 +69,18 @@ class golden_sha;
     _w2 = in.w2;
     _w3 = in.w3;
   endfunction
+*/
 
 
+
+/*
   function new( virtual coreInputsIfc in );
     configure( in );
   endfunction
+*/
 
   
+/*
   function set_hashstate( HashState in );
     _h[0] = in.a;
     _h[1] = in.b;
@@ -58,19 +91,6 @@ class golden_sha;
     _h[6] = in.g;
     _h[7] = in.h;
   endfunction 
-
-
-  function reset();
-    // what else should be emulated here
-
-    _h = {
-      32'h6a09e667, 32'hbb67ae85, 32'h3c6ef372, 32'ha54ff53a, 32'h510e527f, 32'h9b05688c, 32'h1f83d9ab, 32'h5be0cd19
-    };
-  endfunction   
-
-  //function set_h( bit [31:0] h[8] );
-    //_h = h;
-  //endfunction
 
 
   function HashState get_hashstate();
@@ -87,8 +107,9 @@ class golden_sha;
 
     return result;
   endfunction
+*/
 
-
+/*
   function setValid( bit valid );
     _valid = valid;
   endfunction
@@ -103,10 +124,27 @@ class golden_sha;
     configure( in );
     evaluate();
   endfunction
+*/
 
+	
+	local function bit[255:0] array_of_HashState(HashState x);
+		return { x.a, x.b, x.c, x.d, x.e, x.f, x.g, x.h};
+	endfunction
 
-  function evaluate();
+	local function bit[255:0] evaluate(HashState in, bit[31:0] w1, bit[31:0] w2, bit[31:0] w3);
 
+		bit[639:0] msg1;
+		bit[255:0] hs;
+
+		hs = array_of_HashState(in);
+
+		msg1 = { hs, w1, w2, w3, nonce };
+		$display("In: %x; w1: %x; w2: %x; w3: %x; nonce: %x", hs, w1, w2, w3, nonce);
+		$display("Message1: %x", msg1);
+
+		return hs;
+
+/*
     bit message_1[];
     bit[639:0] message_1_bits;
 
@@ -116,6 +154,7 @@ class golden_sha;
     bit [255:0] result2;
 
     $display( "w1=%h, w2=%h, w3=%h", _w1, _w2, _w3 );    
+
 
     if ( _valid && _newBlock )
       _nonce = 0;
@@ -161,13 +200,67 @@ class golden_sha;
     $display( "Second SHA256=%h", result2 );
     
     _result = result2; 
+*/
 
-  endfunction
+	endfunction
 
+/*
   function bit[255:0] getResult();
     // should only make result available after a particular time
     return _result;
   endfunction
+*/
+
+	task cycle();
+		HashState in;
+		bit[31:0] w1, w2, w3;
+		bit[255:0] out;
+
+		// Advance buffers
+		for (int i = DELAY_C; i > 0; i--) begin
+			valid_buf[i] = valid_buf[i - 1];
+			dif_buf[i] = dif_buf[i - 1];
+			hash_buf[i] = hash_buf[i - 1];
+			new_buf[i] = new_buf[i - 1];
+		end
+
+		// Advance nonce
+		if (validIn_i) begin
+			if (newBlockIn_i)
+				nonce = PROCESSORINDEX;
+			else
+				nonce++;
+		end
+
+		// Format input
+		w3 = initialState_i[31:0];
+		w2 = initialState_i[63:32];
+		w1 = initialState_i[95:64];
+
+		in.h = initialState_i[127:96];
+		in.g = initialState_i[159:128];
+		in.f = initialState_i[191:160];
+		in.e = initialState_i[223:192];
+		in.d = initialState_i[255:224];
+		in.c = initialState_i[287:256];
+		in.b = initialState_i[319:288];
+		in.a = initialState_i[351:320];
+
+		// Do SHA Logic
+		out = evaluate(in, w1, w2, w3);
+
+		// Queue data in buffers
+		valid_buf[0] = validIn_i;
+		dif_buf[0] = w3;
+		new_buf[0] = newBlockIn_i;
+		hash_buf[0] = out;
+
+		// Pull outputs from the other side of buffers
+		validOut_o = valid_buf[DELAY_C];
+		difficulty_o = dif_buf[DELAY_C];
+		newBlockOut_o = new_buf[DELAY_C];
+		hash_o = hash_buf[DELAY_C];
+	endtask
 
 endclass
 
