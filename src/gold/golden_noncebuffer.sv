33668087dda89a25315f3e64051b356ed566452f
Rouault <rff2111@columbia.edu>
2011-12-19 16:26:59 -0500

    updated golden nonce buffer

--- src/gold/golden_noncebuffer.sv
+++ src/gold/golden_noncebuffer.sv
@@ -8,13 +8,19 @@ class golden_noncebuffer;
 
 	bit overflow_o;
 	bit nonceOut_o;
+	bit validOut_i;
+	bit successOut_o;
 	
 	bit [31:0] buffer;
 	bit storing = 1'b0;
 	bit clockingout = 1'b0;
 
 	function void noncebuffer_result();
+		validOut_o = validIn_i;
+		successOut_o = successIn_i;
+		
 		if(validIn_i && successIn_i) begin
+
 			storing = 1'b1;
 			if(storing) begin
 				if(clockingout) begin //if second nonce is found before first is clocked out
@@ -29,8 +35,10 @@ class golden_noncebuffer;
 
 		if(readReady_i) begin
 			clockingout = 1'b1;
-			if(clockingout) begin			
-				nonceOut_o = buffer[0];//clock out most recent nonce
+			if(clockingout) begin	
+				for (int i = 0; i < 32; i++) begin		
+					nonceOut_o = buffer[i];//clock out most recent nonce
+				end		
 			end
 			clockingout = 1'b0;
 		end
