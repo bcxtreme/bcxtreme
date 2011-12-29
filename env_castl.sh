4e2d410795869dabc4a95fd0749ed8637bfa6356
Robert Martin <rdm2128@columbia.edu>
2011-12-28 19:49:27 -0500

    Create shortcuts for environment, including 64-bit compiler

---
+++ env_castl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# source /proj/castl/development/synopsys/scripts/env_castl.sh
+source /sim/synopsys/scripts/env.sh
+
---
+++ env_castl64.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# source /proj/castl/development/synopsys/scripts/env_castl.sh
+# source /sim/synopsys/scripts/env.sh
+source /proj/castl/development/synopsys64/env_castl.sh
+
